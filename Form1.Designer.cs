1faa100f3b6e54dca100820b578d66c8c656fe15
Andre Murilo <35697459+andre-murilo@users.noreply.github.com>
2019-05-04 13:54:51 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Proxy_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBMyIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBMyPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBRedirectIP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBRedirectPort = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BTNStart = new System.Windows.Forms.Button();
+            this.LBStatus = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LBClients = new System.Windows.Forms.ListBox();
+            this.BTNKickClient = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.BTNDisconnectAll = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LBStatus);
+            this.groupBox1.Controls.Add(this.BTNStart);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.TBRedirectPort);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.TBRedirectIP);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.TBMyPort);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.TBMyIP);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 167);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Confs";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // TBMyIP
+            // 
+            this.TBMyIP.Location = new System.Drawing.Point(32, 19);
+            this.TBMyIP.Name = "TBMyIP";
+            this.TBMyIP.Size = new System.Drawing.Size(149, 20);
+            this.TBMyIP.TabIndex = 1;
+            this.TBMyIP.Text = "192.168.42.164";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // TBMyPort
+            // 
+            this.TBMyPort.Location = new System.Drawing.Point(234, 19);
+            this.TBMyPort.Name = "TBMyPort";
+            this.TBMyPort.Size = new System.Drawing.Size(73, 20);
+            this.TBMyPort.TabIndex = 3;
+            this.TBMyPort.Text = "7000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Redirect IP: ";
+            // 
+            // TBRedirectIP
+            // 
+            this.TBRedirectIP.Location = new System.Drawing.Point(78, 102);
+            this.TBRedirectIP.Name = "TBRedirectIP";
+            this.TBRedirectIP.Size = new System.Drawing.Size(229, 20);
+            this.TBRedirectIP.TabIndex = 5;
+            this.TBRedirectIP.Text = "158.69.63.181";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Port:  ";
+            // 
+            // TBRedirectPort
+            // 
+            this.TBRedirectPort.Location = new System.Drawing.Point(78, 134);
+            this.TBRedirectPort.Name = "TBRedirectPort";
+            this.TBRedirectPort.Size = new System.Drawing.Size(62, 20);
+            this.TBRedirectPort.TabIndex = 7;
+            this.TBRedirectPort.Text = "80";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(-31, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(556, 10);
+            this.panel1.TabIndex = 8;
+            // 
+            // BTNStart
+            // 
+            this.BTNStart.Location = new System.Drawing.Point(328, 12);
+            this.BTNStart.Name = "BTNStart";
+            this.BTNStart.Size = new System.Drawing.Size(116, 33);
+            this.BTNStart.TabIndex = 9;
+            this.BTNStart.Text = "Start";
+            this.BTNStart.UseVisualStyleBackColor = true;
+            this.BTNStart.Click += new System.EventHandler(this.BTNStart_Click);
+            // 
+            // LBStatus
+            // 
+            this.LBStatus.AutoSize = true;
+            this.LBStatus.Location = new System.Drawing.Point(6, 58);
+            this.LBStatus.Name = "LBStatus";
+            this.LBStatus.Size = new System.Drawing.Size(89, 13);
+            this.LBStatus.TabIndex = 10;
+            this.LBStatus.Text = "Status: Parado ...";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.BTNDisconnectAll);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.BTNKickClient);
+            this.groupBox2.Controls.Add(this.LBClients);
+            this.groupBox2.Location = new System.Drawing.Point(4, 185);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(450, 274);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Clients";
+            // 
+            // LBClients
+            // 
+            this.LBClients.FormattingEnabled = true;
+            this.LBClients.Location = new System.Drawing.Point(6, 19);
+            this.LBClients.Name = "LBClients";
+            this.LBClients.Size = new System.Drawing.Size(301, 238);
+            this.LBClients.TabIndex = 0;
+            // 
+            // BTNKickClient
+            // 
+            this.BTNKickClient.Location = new System.Drawing.Point(313, 19);
+            this.BTNKickClient.Name = "BTNKickClient";
+            this.BTNKickClient.Size = new System.Drawing.Size(137, 27);
+            this.BTNKickClient.TabIndex = 1;
+            this.BTNKickClient.Text = "Kick client";
+            this.BTNKickClient.UseVisualStyleBackColor = true;
+            this.BTNKickClient.Click += new System.EventHandler(this.BTNKickClient_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ban client IP";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // BTNDisconnectAll
+            // 
+            this.BTNDisconnectAll.Location = new System.Drawing.Point(313, 218);
+            this.BTNDisconnectAll.Name = "BTNDisconnectAll";
+            this.BTNDisconnectAll.Size = new System.Drawing.Size(137, 39);
+            this.BTNDisconnectAll.TabIndex = 3;
+            this.BTNDisconnectAll.Text = "Disconnect all";
+            this.BTNDisconnectAll.UseVisualStyleBackColor = true;
+            this.BTNDisconnectAll.Click += new System.EventHandler(this.BTNDisconnectAll_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 462);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(211, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "@ Proxy simples, feito para intuito de testes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 479);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Proxy Server - version 1.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label LBStatus;
+        private System.Windows.Forms.Button BTNStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox TBRedirectPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TBRedirectIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBMyPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBMyIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button BTNDisconnectAll;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button BTNKickClient;
+        private System.Windows.Forms.ListBox LBClients;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Proxy_Server
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 MyForm;
+
+
+        public class XClient
+        {
+            public int id;
+            public Socket s;
+            public const int BufferSize = 4096;
+            public byte[] buff = new byte[BufferSize];
+            public ClientConnector conn;
+
+            public XClient(Socket _s, int _id)
+            {
+                this.s = _s;
+                this.id = _id;
+            }
+        }
+
+        public static List<XClient> Clients = new List<XClient>();
+
+        public XClient GetClientByID(int id)
+        {
+            for(int i =0; i< Clients.Count; i++)
+            {
+                if(Clients[i].id == id)
+                {
+                    return Clients[i];
+                }
+            }
+            return null;
+        }
+
+        public void DisconnectClient(int id)
+        {
+            for (int i = 0; i < Clients.Count; i++)
+            {
+                if (Clients[i].id == id)
+                {
+                    string s_ip = ((IPEndPoint)(Clients[i].s.RemoteEndPoint)).Address.ToString();
+                    int client_id = Clients[i].id;
+                    string conn_addr = Clients[i].conn.ip.Address.ToString();
+                    if (Clients[i].conn.connector.Connected)
+                    {
+                        Clients[i].conn.Disconnect();
+                    }
+                    Clients[i].s.Dispose();
+                    Clients.Remove(Clients[i]);
+                    this.Invoke(new Action(() =>
+                    {
+                        try
+                        {
+                            LBClients.Items.Remove("[" + client_id + "] " + s_ip + " -> " + conn_addr);
+                        }
+                        catch
+                        { return; }
+                    }));
+                }
+            }
+        }
+
+        public static class Info
+        {
+            public static Socket _listener;
+            public const int Buffer_size = 4096;
+            public static byte[] Buffer = new byte[Buffer_size];
+            public static bool Running = false;
+            public static IPEndPoint IP;
+        }
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            MyForm = this;
+        }
+
+        private void BTNStart_Click(object sender, EventArgs e)
+        {
+            if(Info.Running)
+            {
+                Info.Running = false;
+                DisconnectAll();
+                Info._listener.Dispose();
+                LBStatus.Text = "Status: Fechado ...";
+                BTNStart.Text = "Start";
+                return;
+            }
+
+
+            try
+            {
+                IPEndPoint ip = new IPEndPoint(IPAddress.Parse(TBMyIP.Text), int.Parse(TBMyPort.Text));
+                Info.IP = ip;
+            }
+            catch
+            {
+                MessageBox.Show("Erro: IP local ou porta invalidos!");
+                return;
+            }
+
+
+            try
+            {
+                Thread Listen_Thread = new Thread(new ThreadStart(Listen));
+                Listen_Thread.Start();
+            }
+            catch
+            {
+                MessageBox.Show("Erro: Falha ao iniciar socket local!");
+                return;
+            }
+        }
+
+        private void Listen()
+        {
+            try
+            {
+                Info._listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Info._listener.Bind(Info.IP);
+                Info._listener.Listen(25);
+                Info.Running = true;
+                this.Invoke(new Action(() =>
+                {
+                    BTNStart.Text = "Close";
+                    LBStatus.Text = "Status: Aguardando conexões ...";
+                }));
+                Info._listener.BeginAccept(new AsyncCallback(AcceptCallback), Info._listener);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return;
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            if(!Info.Running) { return; }
+
+            Socket s = (Socket)ar.AsyncState;
+            try
+            {
+                Socket s_client = s.EndAccept(ar);
+                int client_id = s_client.GetHashCode();
+
+                Clients.Add(new XClient(s_client, client_id));
+
+
+                XClient client = GetClientByID(client_id);
+                if(client == null) { return; }
+                //MessageBox.Show("Client conectado: " + client_id.ToString());
+
+                string ip_redirect = TBRedirectIP.Text;
+
+                Uri uriResult;
+                IPAddress ip;
+                bool result = Uri.TryCreate(ip_redirect, UriKind.Absolute, out uriResult)
+                    && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+                if (result)
+                {
+                    ip = Dns.GetHostAddresses(uriResult.Host)[0];
+                }
+                else
+                {
+                    IPAddress.TryParse(ip_redirect, out ip);
+                }
+
+                IPEndPoint direct = new IPEndPoint(ip, int.Parse(TBRedirectPort.Text));
+
+                ClientConnector conn = new ClientConnector(client, direct);
+                client.conn = conn;
+
+                Thread.Sleep(600);
+                this.Invoke(new Action(() =>
+                {
+                    string s_ip = ((IPEndPoint)(client.s.RemoteEndPoint)).Address.ToString();
+                    LBClients.Items.Add("[" + client.id + "] " + s_ip + " -> " + client.conn.ip.Address.ToString());
+                }));
+
+                client.s.BeginReceive(client.buff, 0, XClient.BufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), client);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return;
+            }
+            finally
+            {
+                if (Info.Running)
+                {
+                    s.BeginAccept(new AsyncCallback(AcceptCallback), s);
+                }
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            XClient client = (XClient)ar.AsyncState;
+            try
+            {
+                if(client == null) { return; }
+                if(client.s == null) { return; }
+                if (!client.s.Connected) { return; }
+
+                int bytes_receive = client.s.EndReceive(ar);
+
+                if(bytes_receive > 0)
+                {
+                    byte[] data = new byte[bytes_receive];
+                    Array.Copy(client.buff, data, bytes_receive);
+
+                    HandlePacket(client, data);
+                    client.buff = new byte[XClient.BufferSize];
+                    client.s.BeginReceive(client.buff, 0, XClient.BufferSize, SocketFlags.None, new AsyncCallback(ReceiveCallback), client);
+                }
+                else
+                {
+                    // disconnect client
+                    DisconnectClient(client.id);
+                    return;
+                }
+            }
+            catch
+            {
+                // disconnect client; error operand
+                DisconnectClient(client.id);
+                return;
+            }
+        }
+
+
+        public void Send(XClient client, byte[] data)
+        {
+            try
+            {
+                if(client.s.Connected)
+                    client.s.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), client.s);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            Socket s = (Socket)ar.AsyncState;
+            try
+            {
+                s.EndSend(ar);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void HandlePacket(XClient client, byte[] data)
+        {
+            try
+            {
+                client.conn.Send(data);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void BTNKickClient_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(LBClients.SelectedItem == null) { return; }
+                string client_id = (string)LBClients.SelectedItem;
+                string parsed = client_id.Split('[')[1];
+                string parsed2 = client_id.Split(']')[0];
+                string parsed3 = parsed2.Replace("[", "");
+                int client = int.Parse(parsed3);
+                DisconnectClient(client);
+            }
+            catch(Exception er)
+            {
+                MessageBox.Show(er.ToString());
+                return;
+            }
+        }
+
+        private void BTNDisconnectAll_Click(object sender, EventArgs e)
+        {
+            DisconnectAll();
+        }
+
+        private void DisconnectAll()
+        {
+            for (int i = 0; i < Clients.Count; i++)
+            {
+                DisconnectClient(Clients[i].id);
+            }
+        }
+
+        // CLIENT --------------------------------------------------------
+       /*
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * 
+        * */
+
+        public class ClientConnector
+        {
+            public XClient client;
+            public IPEndPoint ip;
+            public Socket connector;
+
+            public const int buffer_size = 4096;
+            private byte[] buffer = new byte[buffer_size];
+
+
+            public void Disconnect()
+            {
+                try
+                {
+                    connector.Close();
+                    connector.Dispose();
+                }
+                catch
+                {
+                    return;
+                }
+            }
+
+
+            public ClientConnector(XClient _client, IPEndPoint _ip)
+            {
+                this.client = _client;
+                this.ip = _ip;
+
+                try
+                {
+                    connector = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    connector.BeginConnect(ip, new AsyncCallback(ConnectCallback), connector);
+                }
+                catch
+                {
+                    return;
+                }
+
+            }
+
+            private void ConnectCallback(IAsyncResult ar)
+            {
+                Socket s = (Socket)ar.AsyncState;
+                try
+                {
+                    s.EndConnect(ar);
+
+                    s.BeginReceive(buffer, 0, buffer_size, SocketFlags.None, new AsyncCallback(ReceiveCB), s);
+                }
+                catch
+                {
+                    return;
+                }
+            }
+
+
+            public void Send(byte[] data)
+            {
+                try
+                {
+                    if(connector.Connected)
+                    {
+                        connector.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), connector);
+                    }
+                }
+                catch
+                {
+                    return;
+                }
+            }
+            private void SendCallback(IAsyncResult ar)
+            {
+                try
+                {
+                    Socket s = (Socket)ar.AsyncState;
+                    s.EndSend(ar);
+                }
+                catch
+                {
+                    return;
+                }
+            }
+
+            private void ReceiveCB(IAsyncResult ar)
+            {
+                Socket s = (Socket)ar.AsyncState;
+                try
+                {
+                    if (!s.Connected) { return; }
+
+                    int received_bytes = s.EndReceive(ar);
+                    if(received_bytes > 0)
+                    {
+                        byte[] data = new byte[received_bytes];
+                        Array.Copy(buffer, data, received_bytes);
+                        MyForm.Send(client, data);
+                        s.BeginReceive(buffer, 0, buffer_size, SocketFlags.None, new AsyncCallback(ReceiveCB), s);
+                    }
+                }
+                catch
+                {
+                    return;
+                }
+            }
+
+
+
+        }
+
+
+
+    }
+}
